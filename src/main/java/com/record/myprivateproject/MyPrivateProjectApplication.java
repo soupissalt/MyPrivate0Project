a5ff9c04567f20d168f54bea6df1e207d9db2b90
CL <dlackagh@gmail.com>
2025-07-25 18:32:13 +0900

    test commit

---
+++ src/main/java/com/record/myprivateproject/MyPrivateProjectApplication.java
@@ -0,0 +1,13 @@
+package com.record.myprivateproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPrivateProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyPrivateProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/record/myprivateproject/MyPrivateProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.record.myprivateproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPrivateProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
